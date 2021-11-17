674848e7786ffce86f9edda02c1709530c1ccaad
ranemihir <mihirrane171@gmail.com>
2021-11-17 18:36:49 +0530

    minor style changes

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 import { Channel } from './../types';
 
 export const ChannelCard = (props: { channel: Channel; }) => {
     return (
-        <div className='card my-3 rounded-0'>
+        <div className='card bg-light my-3 rounded-0 shadow-sm'>
             <div className='d-flex flex-row justify-content-between align-items-center'>
                 <img className='img-thumbnail rounded-circle m-3 shadow-sm' src={props.channel.thumbnailUrl} alt={props.channel.name + ' thumbnail'} />
                 <div className='card-body pe-4'>
-                    <h5 className='card-title'>{props.channel.name}</h5>
+                    <Link className='text-black text-decoration-none' to={'/' + props.channel.id}>
+                        <h5 className='card-title'>{props.channel.name}</h5>
+                    </Link>
                     <p className='card-text'>{props.channel.description}</p>
                     <p className='card-text'>
                         <small className='text-muted'>{new Date(props.channel.publishedAt).toLocaleDateString(undefined, {
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,14 +25,14 @@ export const Home = (props: { authToken: string | null; }) => {
     };
 
     return (
-        <div className="row mt-5">
-            <div className="col-12">
+        <div className="row mt-5 justify-content-center">
+            <div className="col-md-8 ">
                 <form className="d-flex" onSubmit={handleSubmit}>
-                    <input onChange={handleChange} value={query} className="form-control me-2 rounded-0" type="search" placeholder="Channel Name" aria-label="Search" />
-                    <button className="btn btn-outline-primary rounded-0" type="submit">Search</button>
+                    <input onChange={handleChange} value={query} className="form-control me-2 rounded-0 bg-light" type="search" placeholder="Channel Name" aria-label="Search" />
+                    <button className="btn btn-outline-danger rounded-0" type="submit">Search</button>
                 </form>
             </div>
-            <div className="col-12">
+            <div className="col-md-8">
                 <div className="my-5">
                     {
                         channels.map((channel: Channel) => (
